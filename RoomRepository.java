259afb2bc6233117984a8550fa8e6ae42b696094
RSekha <146330142+RSekha01@users.noreply.github.com>
2025-09-12 13:46:27 +0530

    Create RoomRepository.java

---
+++ RoomRepository.java
@@ -0,0 +1,9 @@
+package com.example.hotelmanagement.repository;
+
+import com.example.hotelmanagement.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByRoomNumber(String roomNumber);
+}
