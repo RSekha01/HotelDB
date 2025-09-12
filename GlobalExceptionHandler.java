101def13f47a445d0bce2980d1b0a539f0cc3a06
RSekha <146330142+RSekha01@users.noreply.github.com>
2025-09-12 11:47:51 +0530

    Create GlobalExceptionHandler.java

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.hotelmanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAll(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of(
+                        "timestamp", LocalDateTime.now(),
+                        "message", ex.getMessage()
+                ));
+    }
+}
